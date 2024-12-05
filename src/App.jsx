bc434cd527e399bca4f784c68f2a4be7a768720c
Disha <sethidisha123@gmail.com>
2024-12-05 15:57:37 +0530

    added the data in the table by fetching api data using axios and made the footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Coin from './pages/Coin/Coin';
-
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -15,6 +15,8 @@ const App = () => {
 
         <Route path='/coin/:coinId' element={<Coin/>}/>
       </Routes>
+
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @ 2024, Cryptopalace-All Rights Reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
